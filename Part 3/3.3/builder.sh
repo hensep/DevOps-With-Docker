6a3ca82aa24e4ad5f3112eef160afa8de18ef295
Henrik Seppänen <henrik.seppanen.a.s@gmail.com>
2025-01-17 16:27:39 +0200

    parts 3.2-3.3

---
+++ Part 3/3.3/builder.sh
@@ -0,0 +1,26 @@
+GITHUB_REPO=$1
+DOCKER_HUB_REPO=$2
+
+REPO_NAME=$(basename -s .git "$GITHUB_REPO")
+
+# Clone the GitHub repository
+echo "Cloning GitHub repository: $GITHUB_REPO"
+git clone https://github.com/"$GITHUB_REPO"
+
+# Navigate to the repository directory
+cd "$REPO_NAME" || exit
+
+#Build the Docker image
+echo "Building Docker image..."
+docker build . -t "$DOCKER_HUB_REPO"
+
+echo "Pushing Docker image to Docker Hub"
+# Push the Docker image to Docker Hub
+docker push "$DOCKER_HUB_REPO"
+
+echo "Successfully built and pushed Docker image to $DOCKER_HUB_REPO"
+
+# Remove the created directory
+cd ..
+echo "Removing the created directory"
+rm -rf "$REPO_NAME"
